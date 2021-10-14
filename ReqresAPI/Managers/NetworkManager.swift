3acef83b1c89759a9ed575fa169bae36a0c82d8a
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-10-14 15:45:41 +0200

    Added CustomErrors enum and created basic network call inside NetworkManager

--- ReqresAPI/Managers/NetworkManager.swift
+++ ReqresAPI/Managers/NetworkManager.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class NetworkManager{
+    //MARK: - Properties
+    static let shared = NetworkManager()
+    private let endpoint = "https://reqres.in/api/users"
+    
+    private init() {}
+    
+    func getUsersData(from endpoint: String, completed: @escaping (Result<User, CustomErrors>) -> Void){
+        guard let url = URL(string: endpoint) else{
+            completed(.failure(.unableToComplete))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error{
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else{
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let users = try decoder.decode(User.self, from: data)
+                completed(.success(users))
+            }catch{
+                completed(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
+}
--- ReqresAPI/Utilities/CustomErrors.swift
+++ ReqresAPI/Utilities/CustomErrors.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+enum CustomErrors: String, Error{
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your Internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
