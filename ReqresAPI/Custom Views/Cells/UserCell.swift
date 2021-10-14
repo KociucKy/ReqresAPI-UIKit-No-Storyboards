b9e25e605e77ea9e9e5e41f3cd6422b47c1a3bec
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-10-14 16:36:32 +0200

    Configured the custom UITableViewCell

--- ReqresAPI/Custom Views/Cells/UserCell.swift
+++ ReqresAPI/Custom Views/Cells/UserCell.swift
@@ -9,15 +9,37 @@ import UIKit
 
 class UserCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    //MARK: - UI Elements
+    static let reuseID = "UserCell"
+    let fullName = CustomTitleLabel(fontSize: 20, textAlignment: .left)
+    
+    
+    //MARK: - Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    
+    //MARK: - Methods
+    private func configure(){
+        self.accessoryType = .disclosureIndicator
+        addSubview(fullName)
+        fullName.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            fullName.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            fullName.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12),
+            fullName.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12),
+            fullName.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
+        ])
+    }
+    
+    
+    func set(list: [UserData], index: Int){
+        fullName.text = list[index].first_name + " " + list[index].last_name
+    }
 }
